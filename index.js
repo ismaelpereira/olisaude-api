58def0d9d3aa93e3ca0648223150e4dfb145bd7f
Ismael Pereira <ismaelfgpereira@gmail.com>
2022-04-05 17:50:05 -0300

    migrate routes to index and added nodemon

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("Hello World");
+import express from "express";
+
+export const app = express();
+
+app.use(express.json());
+
+app.get("/", function (req, res) {
+  res.send({
+    message: "hello world",
+  });
+});
+app.get("/client/:id", function (req, res) {});
+app.post("/client", function (req, res) {});
+app.put("/client/:id", function (req, res) {});
+app.delete("/client/:id", function (req, res) {});
+app.get("/clients/risk", function (req, res) {});
+
+app.listen("4000", () => console.log("api listening on port 4000"));
